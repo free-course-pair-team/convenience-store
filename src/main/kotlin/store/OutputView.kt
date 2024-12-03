ca07e93de9fceadeca6667629951967ab1ba660c
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-03 13:20:24 +0900

    feat(OutputView): 보유하고 있는 상품 출력 기능 추가

--- src/main/kotlin/store/OutputView.kt
+++ src/main/kotlin/store/OutputView.kt
@@ -1,7 +1,21 @@
 package store
 
+import java.text.DecimalFormat
+
 class OutputView {
     fun printStart() {
         println("안녕하세요. W편의점입니다.")
+    }
+
+    fun printProducts(products: List<Product>) {
+        println("현재 보유하고 있는 상품입니다.")
+        products.forEach { product ->
+            println("- ${product.name} ${product.price.wonFormat()}원 ${product.quantity}개 ${product.promotion ?: ""}")
+        }
+    }
+
+    private fun Int.wonFormat(): String {
+        val formatter = DecimalFormat("#,###")
+        return formatter.format(this)
     }
 }
