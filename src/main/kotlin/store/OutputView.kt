f8f76bf38726fb75c13799cad9e8ee21fb39eb31
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-03 14:01:10 +0900

    debug(OutputView): 개수가 0개일 경우 재고 없음 처리

--- src/main/kotlin/store/OutputView.kt
+++ src/main/kotlin/store/OutputView.kt
@@ -10,12 +10,17 @@ class OutputView {
     fun printProducts(products: List<Product>) {
         println("현재 보유하고 있는 상품입니다.")
         products.forEach { product ->
-            println("- ${product.name} ${product.price.wonFormat()}원 ${product.quantity}개 ${product.promotion ?: ""}")
+            println("- ${product.name} ${product.price.wonFormat()}원 ${product.quantity.zeroToSoldOut()} ${product.promotion ?: ""}")
         }
     }
 
     private fun Int.wonFormat(): String {
         val formatter = DecimalFormat("#,###")
         return formatter.format(this)
+    }
+
+    private fun Int.zeroToSoldOut(): String {
+        if (this == 0) return "재고 없음"
+        return this.toString() + "개"
     }
 }
