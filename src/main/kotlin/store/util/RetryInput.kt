c5cfaf53fc9a42d59a3e05e45125baf6f5c788f1
DaeYoung Jeong <eodud1025@naver.com>
2024-12-08 01:03:20 +0900

    fix(retryInput): 오류발생시 입력 버터에 개행문자 남아있는 이슈 해결

--- src/main/kotlin/store/util/RetryInput.kt
+++ src/main/kotlin/store/util/RetryInput.kt
@@ -1,11 +1,15 @@
 package store.util
 
+import camp.nextstep.edu.missionutils.Console
+
 fun <T>retryInput(input: () -> T): T {
     while (true) {
         try {
             return input()
         } catch (e: Exception) {
-            println("Error: ${e.message}")
+            e.printStackTrace()
+            println(e.message)
+            Console.readLine() // 입력 버퍼에 남아 있는 개행문자 제거
         }
     }
 }
